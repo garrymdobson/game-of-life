715b05f7e6d047d75452611382510989b93bec9c
Garry Dobson <garrymdobson@linux.suse>
2017-12-07 16:57:33 +1100

    Changed Cell.java the second time for testing Jenkins pipeline

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -7,7 +7,8 @@ package com.wakaleo.gameoflife.domain;
  * Cell.java
  * Cell object class, contains information on individual cells (positions) in the grid
  */
-// add a comment for testing Jenkins - GMD 6Dec17
+// add a comment for testing Jenkins        - GMD 6Dec17
+// add a second comment for testing Jenkins - GMD 7Dec17
 // 
 public enum Cell {
 	// Symbols to represent cell status
