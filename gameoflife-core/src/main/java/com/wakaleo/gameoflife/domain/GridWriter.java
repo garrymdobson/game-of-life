598fc2be0e8b1cc7c5eba72c21bd79f51723d4ee
Gogs <gogs@fake.local>
2019-06-28 13:49:33 +1000

    Changed GridWriter.java the ninth time for testing Jenkins pipeline web-hook in AWS

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/GridWriter.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/GridWriter.java
@@ -16,6 +16,7 @@ package com.wakaleo.gameoflife.domain;
 // add a sixth   comment for testing Jenkins pipeline web-hook in AWS - GMD 20June19
 // add a seventh comment for testing Jenkins pipeline web-hook in AWS - GMD 21June19
 // add an eighth comment for testing Jenkins pipeline web-hook in AWS - GMD 21June19
+// add a ninth   comment for testing Jenkins pipeline web-hook in AWS - GMD 28June19
 //
 public class GridWriter {
 
