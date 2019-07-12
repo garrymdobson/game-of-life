bf2216517cc75bd7b251565f9ae313fc2eaee567
Gogs <gogs@fake.local>
2019-07-12 15:34:19 +1000

    Changed GridReader.java the eighth time for testing Jenkins pipeline web-hook in AWS

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/GridReader.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/GridReader.java
@@ -18,6 +18,7 @@ import java.util.List;
 // add a fifth   comment for testing Jenkins pipeline Web-hook - GMD 6Feb18
 // add a sixth   comment for testing Jenkins pipeline Web-hook - GMD 7Feb18
 // add a seventh comment for testing Jenkins pipeline Web-hook - GMD 20Feb18
+// add an eighth comment for testing Jenkins pipeline web-hook in AWS - GMD 12Jul19
 //
 public class GridReader {
 
